365a050b2c6a85e8e6e08e20bc9a5e6473bd1d8a
HavocsCall <HavocsCalls@gmail.com>
2025-06-17 10:35:46 -0400

    # 1.0.0 ## Added ### Fill Tags with Properties - Fill Tags with Properties on Current File - Fill Tags with Properties All Files on All Files ### Misc - Clear All Tags on Current File - Clear All Tags All Files on All Files

--- main.ts
+++ main.ts
@@ -1,134 +1,290 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, PluginSettingTab, App, Setting, TFile  } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+// ----------------------------------------------------------------------------------------------------//
+// -----Settings-----
+interface propertyBaronSettings {
+    propertyList: string;
+    noPrefixProperties: string;
+    propertyListMode: 'Whitelist' | 'Blacklist';
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: propertyBaronSettings = {
+    propertyList: '',
+    noPrefixProperties: '',
+    propertyListMode: 'Whitelist'
+};
+
+class propertyBaronSettingsTab extends PluginSettingTab {
+    plugin: propertyBaron;
+
+    constructor(app: App, plugin: propertyBaron) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+
+        // Fill Tags with Properties Settings
+        containerEl.createEl('h1', { text: 'Fill Tags with Properties' });
+        
+        new Setting(containerEl)
+            .setName('Property List Mode')
+            .setDesc('Choose whether the property list acts as a blacklist (exclude) or whitelist (only include)')
+            .addDropdown(drop => drop
+                .addOption('Whitelist', 'Whitelist (only include these properties)')
+                .addOption('Blacklist', 'Blacklist (exclude these properties)')
+                .setValue(this.plugin.settings.propertyListMode)
+                .onChange(async (value: 'Whitelist' | 'Blacklist') => {
+                    this.plugin.settings.propertyListMode = value;
+                    await this.plugin.saveSettings();
+                })
+            );
+
+        new Setting(containerEl)
+            .setName('Property List')
+            .setDesc('Comma-separated list of properties')
+            .addText(text => text
+                .setPlaceholder('')
+                .setValue(this.plugin.settings.propertyList)
+                .onChange(async (value) => {
+                    this.plugin.settings.propertyList = value;
+                    await this.plugin.saveSettings();
+                })
+        );
+        
+        new Setting(containerEl)
+            .setName('No Prefix Properties')
+            .setDesc('Comma-separated list of properties to add as tags without the PropertyName/ prefix. Be aware that certain tags will still have prefixes in order to make them valid tags. These mostly include "numbers only" tags.')
+            .addText(text => text
+                .setPlaceholder('')
+                .setValue(this.plugin.settings.noPrefixProperties)
+                .onChange(async (value) => {
+                    this.plugin.settings.noPrefixProperties = value;
+                    await this.plugin.saveSettings();
+                })
+        );
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// ----------------------------------------------------------------------------------------------------//
+// -----Main Plugin Class-----
+export default class propertyBaron extends Plugin {
+    settings: propertyBaronSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
+        this.addSettingTab(new propertyBaronSettingsTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        //--------------------------------------------------//
+        // ---Add Commands---
+        this.addCommand({
+            id: 'fill-tags-with-properties',
+            name: 'Fill Tags with Properties on Current File',
+            callback: () => {
+                const file = this.app.workspace.getActiveFile()
+                if (file) {
+                    this.fillTagsWithProperties(file);
+                }
+            }
+            
+        });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.addCommand({
+            id: 'fill-tags-with-properties-all-files',
+            name: 'Fill Tags with Properties All Files on All Files',
+            callback: () => {
+                this.fillTagsWithPropertiesAllFiles(this.app);
+            }            
+        });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        this.addCommand({
+            id: 'clear-all-tags',
+            name: 'Clear All Tags on Current File',
+            callback: () => {
+                const file = this.app.workspace.getActiveFile()
+                if (file) {
+                    this.clearAllTags(file);
+                }
+            } 
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        this.addCommand({
+            id: 'clear-all-tags-all-files',
+            name: 'Clear All Tags All Files on All Files',
+            callback: () => {
+                this.clearAllTagsAllFiles(this.app);
+            }            
+        });
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    //--------------------------------------------------//
+    // ---Functions---
+    // -Load Settings
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    // -Save Settings-
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // -Clear All Tags-
+    async clearAllTags(file: TFile) {
+        await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+            const tagsKey = Object.keys(frontmatter).find(k => k.toLowerCase() === 'tags');
+            
+            if (tagsKey) {
+                frontmatter[tagsKey] = [];
+            } else {
+                frontmatter['tags'] = [];
+            }
+        });
+    }
 
-	onunload() {
+    // -Clear All Tags All Files-
+    async clearAllTagsAllFiles(app: App) {
+        new Notice('Starting to clear all tags across your vault')
+        await Promise.all(app.vault.getMarkdownFiles().map(async (file) => {
+            try {
+                await this.clearAllTags(file);
+            } catch (error) {
+                new Notice(error);
+            }
+        }));
+        new Notice("Job's Done")
+    }
 
-	}
+    // -Fill Tags With Properties-
+    async fillTagsWithProperties(file: TFile) {
+        // Load Settings
+        const propertyListMode = this.settings.propertyListMode;
+        const propertyList = this.settings.propertyList
+            .split(',')
+            .map(p => p.trim().toLowerCase())
+            .filter(Boolean);
+        
+        switch (propertyListMode) {
+            case 'Blacklist': {
+                if (!propertyList.includes('tags')) {
+                    propertyList.push('tags');
+                }
+            }
+            break;
+            case 'Whitelist': {
+                const tagsIndex = propertyList.indexOf('tags');
+                if (tagsIndex !== -1) {
+                    propertyList.splice(tagsIndex, 1);
+                }
+            }
+            break;
+        }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+        const noPrefixProperties = this.settings.noPrefixProperties
+            .split(',')
+            .map(p => p.trim().toLowerCase())
+            .filter(Boolean);
+        
+        // Process Frontmatter
+        await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+            const tagsKey = Object.keys(frontmatter).find(k => k.toLowerCase() === 'tags');
+            const currentTags = tagsKey ? frontmatter[tagsKey] : [];
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+            let filteredProperties: string[];
+            switch (propertyListMode) {
+                case 'Blacklist': {
+                    filteredProperties = Object.keys(frontmatter)
+                        .filter(key => !propertyList.includes(key.toLowerCase()));
+                }
+                break;
+                case 'Whitelist': {
+                    filteredProperties = Object.keys(frontmatter)
+                        .filter(key => propertyList.includes(key.toLowerCase()));
+                }
+                break;
+            }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+            const newTags =[];
+            
+            for (const property of filteredProperties) {
+                const value = frontmatter[property];
+                const isNoPrefix = noPrefixProperties.includes(property.toLowerCase());
+                
+                if (value != null) {
+                    switch (typeof value) {
+                        case 'object': {
+                            for (const eachValue of value) {
+                                let cleanedValue = eachValue;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+                                const wikiMatch = /^\[\[[^\|\]]+\|([^\]]+)\]\]$/.exec(cleanedValue);
+                                if (wikiMatch) {
+                                    cleanedValue = wikiMatch[1];
+                                }
+                                cleanedValue = cleanedValue.replace(/\s+/g, "_");
+                                cleanedValue = cleanedValue.replace(/[^a-zA-Z0-9_-]/g, "");
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+                                if (/[a-zA-Z_-]/.test(cleanedValue)) {
+                                    newTags.push(isNoPrefix ? cleanedValue : `${property}/${cleanedValue}`);
+                                } else {
+                                    newTags.push(`${property}/${cleanedValue}`);
+                                }
+                            }
+                            break;
+                        }
+                        case 'string': {
+                            let cleanedValue = value;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+                            const wikiMatch = /^\[\[[^\|\]]+\|([^\]]+)\]\]$/.exec(cleanedValue);
+                            if (wikiMatch) {
+                                cleanedValue = wikiMatch[1];
+                            }
+                            cleanedValue = cleanedValue.replace(/\s+/g, "_");
+                            cleanedValue = cleanedValue.replace(/[^a-zA-Z0-9_-]/g, "");
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+                            if (/[a-zA-Z_-]/.test(cleanedValue)) {
+                                newTags.push(isNoPrefix ? cleanedValue : `${property}/${cleanedValue}`);
+                            } else {
+                                newTags.push(`${property}/${cleanedValue}`);
+                            }
+                            break;
+                        }
+                        case 'number': {
+                            let cleanedValue = String(value).replace(/\./g, "_");
+                            newTags.push(`${property}/${cleanedValue}`);
+                            break;
+                        }
+                        case 'boolean': {
+                            newTags.push(isNoPrefix ? value : `${property}/${value}`);
+                            break;
+                        }
+                    }
+                }
+            }
 
-	display(): void {
-		const {containerEl} = this;
+            const mergedTags = Array.from(new Set([
+                ...(Array.isArray(currentTags) ? currentTags : []),
+                ...(Array.isArray(newTags) ? newTags : [])
+            ]));
 
-		containerEl.empty();
+            if (tagsKey) {
+                frontmatter[tagsKey] = mergedTags;
+            } else {
+                frontmatter['tags'] = mergedTags;
+            }
+        });
+    }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    // -Fill Tags with Properties All Files-
+    async fillTagsWithPropertiesAllFiles(app: App) {
+        new Notice('Starting to fill tags with properties across your vault')
+        await Promise.all(app.vault.getMarkdownFiles().map(async (file) => {
+            try {
+                await this.fillTagsWithProperties(file);
+            } catch (error) {
+                new Notice(error);
+            }
+        }));
+        new Notice("Job's Done")
+    }
 }
